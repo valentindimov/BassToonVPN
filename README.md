f73c75d6bca32043d3ceaacd2d9fce84c1a6f2f9
Valentin Dimov <balentin.dimov@gmail.com>
2025-08-02 13:13:10 +0200

    feat: make the installation process two-step, with the script running on the user's PC
    Signed-off-by: Valentin Dimov <balentin.dimov@gmail.com>

--- README.md
+++ README.md
@@ -1,33 +1,45 @@
 # BassToonVPN
-BassToonVPN is a relatively simple interactive tool for setting up WireGuard VPN servers:
-- Configures NAT masquerading (clients will access the Internet through the server's IP address)
-- Supports the initial installation and the addition of new clients (up to 253 in total)
-- Requires only standard Linux utilites and the `wireguard-tools` package.
+BassToonVPN is an interactive tool for setting up hub-and-spoke WireGuard VPNs:
+- Configures NAT masquerading (clients will access the Internet through the server's IP address);
+- Supports the initial installation and the addition of new clients (up to 254 in total);
+- Requires only a few common Linux utilites;
 - It can also print QR codes to your terminal for setting up mobile devices.
 
 BassToonVPN is meant for technical enthusiasts who are comfortable with the command line and are looking for a simple setup with minimal configuration necessary - all you need is a Linux virtual or physical machine with a public IP address and SSH (or console) access.
 For a more comprehensive solution with a friendly Web-based UI, you probably want [wg-easy](https://github.com/wg-easy/wg-easy).
 
 ## Usage
-On the system which will be your VPN server, download and run the script with root privileges and follow the interactive prompts:
-```bash
-curl -s https://raw.githubusercontent.com/valentindimov/BassToonVPN/refs/heads/main/basstoonvpn.sh > basstoonvpn.sh
-chmod +x basstoonvpn.sh
-sudo ./basstoonvpn.sh
-```
-The script can perform the initial configuration on the VPN server, or add new clients to an already-installed configuration.
+1. Set up an SSH key on your system: `ssh-keygen`
+2. Configure your SSH key for access to a root user on the remote machine
+3. Download, install, and run the script and follow the interactive prompts:
+    ```bash
+    curl -s https://raw.githubusercontent.com/valentindimov/BassToonVPN/refs/heads/main/basstoonvpn.sh > /usr/local/bin/basstoonvpn
+    sudo chown root:root /usr/local/bin/basstoonvpn
+    sudo chmod 0555 /usr/local/bin/basstoonvpn
+    basstoonvpn
+    ```
+
+To add more clients to an already-installed VPN server, simply run `basstoonvpn` and follow the interactive prompts again.
 
 ### WARNING: BassToonVPN is designed for dedicated servers!
-BassToonVPN is designed to run on a dedicated device or VM.
-It may install required dependencies, uninstall packages that can conflict with it, and it will completely overwrite the host's firewall configuration.
+BassToonVPN assumes the remote server is a dedicated physical device or VM.
+It can install required dependencies, uninstall packages that can conflict with it, and it will completely overwrite the server's firewall configuration.
 
 ### SSH after setup
 BassToonVPN configures the host's firewall such that only the WireGuard service (UDP port 51820) is publicly accessible.
 It also allows SSH connections to the host, but only from inside the VPN.
-If you want to SSH to the host after initially installing the VPN, you need activate your VPN connection and connect to `192.168.103.1`.
+If you want to SSH to the host after initially installing the VPN, you need to activate your VPN connection and connect to `192.168.103.1`.
 
 ## Requirements
-Currently BassToonVPN supports Debian or Ubuntu systems and requires `curl`, `wireguard-tools` (more specifically the `wg-quick` utility), and `nftables` to be installed on the system. `qrencode` is also used for printing QR codes (useful e.g. for setting mobile clients).
-When the VPN is first set up, it will try to install these dependencies itself, and it will also remove the `iptables` and `firewalld` packages if they are installed.
+Currently BassToonVPN supports Debian or Ubuntu systems and requires `curl`, `wireguard-tools` (more specifically the `wg-quick` utility), and `nftables` to be installed on the remote server. `qrencode` needs to be installed locally to display QR codes..
+
+When the VPN is first set up on the remote server, dependencies will be installed, and the `iptables` and `firewalld` packages will be removed if they are installed since they can interfere with its firewall configuration.
 
 The script can be ported to other Linux distros rather easily, you just need to replace the `apt-get` commands with the corresponding alternative on your system.
+
+## Future functionality
+The following will be worked on in the future:
+- Output the client configuration into a .conf file
+- List clients in the VPN
+- Delete clients from the VPN
+- Extend the number of possible clients
--- basstoonvpn.sh
+++ basstoonvpn.sh
@@ -1,250 +1,191 @@
 #!/usr/bin/env bash
-# SPDX-License-Identifier: MIT
 
-# Stop the script if any error occurs
 set -eo pipefail
 
-# Make sure that we are running with administrative privileges (we'll need to install things on the system, after all)
-if [ "$UID" != "0" ]
+# Helper function that prints a separator to make things look nicer
+print_separator() {
+	echo ""
+	echo "========================================"
+	echo ""
+}
+
+# Process command-line args and inputs
+REMOTE_SSH_ADDR=$1
+ADDITION_MODE=$2
+if [[ -z "$REMOTE_SSH_ADDR" ]]
 then
-	echo "Error: Please run this script as root (type: \"sudo $0\")"
-	exit 1
+	read -p "Remote user and address? (e.g. root@123.45.67.89): " REMOTE_SSH_ADDR
+else
+	echo "Connecting to $REMOTE_SSH_ADDR."
 fi
-
-# Print warning that we'll be messing with system packages and firewall settings
-echo "==================================================="
-echo "BassToonVPN Setup"
-echo "WARNING: This script is designed to be run on a bare, dedicated device or VM."
-echo "It will install and uninstall packages from the system, and it will reset and reconfigure its firewall rules."
-echo "==================================================="
-
-DO_PROCEED=""
-until [ "$DO_PROCEED" = "y" -o "$DO_PROCEED" = "n" ]
+while [[ "$ADDITION_MODE" != "add" && "$ADDITION_MODE" != "reset" ]]
 do
-	read -p "Proceed with BassToonVPN setup? (y/n): " DO_PROCEED
+	read -p "Mode? (add/reset): " ADDITION_MODE
 done
-if [ "$DO_PROCEED" != "y" ]
+
+# Reset mode: uninstalls the VPN and removes firewall rules
+if [[ "$ADDITION_MODE" == "reset" ]]
 then
-	echo "Cancelled."
-	exit 1
+	print_separator
+	echo "Uninstalling the VPN. If you connected through the VPN, your connection might drop here."
+	print_separator
+	# Wrap this in a single bask call to force the remote to fetch the entire command before executing.
+	# This should prevent the command executing only halfway due to the connection dropping
+	ssh -2 "$REMOTE_SSH_ADDR" "bash -c \"nft flush ruleset; rm /etc/wireguard/basstoonvpn.conf; systemctl disable --now wg-quick@basstoonvpn.service; ip link del basstoonvpn\"" || true
+	print_separator
+	exit 0
 fi
 
-# ID of the client to be created, a number between 2 and 255.
-CLIENT_ID="2"
-DELETE_VPN_CONF="no"
+# Otherwise, we're in add mode and we will install or extend the VPN on the remote
 
-# If a /etc/wireguard/basstoonvpn.conf already exists...
-if [ -f /etc/wireguard/basstoonvpn.conf ]
-then
-	echo ""
-	echo "==================================================="
-	echo "Existing VPN configuration found."
-	echo "==================================================="
-	echo ""
-	echo "There is already a VPN configuration installed."
-	echo "Would you like to:"
-	echo "1) Delete the old VPN and create a new one"
-	echo "2) Add a new device to the existing VPN"
-	echo "c) Cancel the installation"
-	CHOSEN_OPTION=""
-	until [ "$CHOSEN_OPTION" = "1" -o "$CHOSEN_OPTION" = "2" -o "$CHOSEN_OPTION" = "c" ] # 
-	do
-		read -p "Specify an option and press Enter (1, 2, c): " CHOSEN_OPTION
-	done
+# This function will be sent to the remote and executed. It manages the WireGuard config file.
+add_wireguard_client() {
+	local CLIENT_PUB_KEY=$1
+	if [[ -z $CLIENT_PUB_KEY ]]
+	then
+		echo "Usage: $0 <client public key>" 1>&2
+		return 1
+	fi
 
-	if [ "$CHOSEN_OPTION" == "c" ]
+	# ID of the client to be created, a number between 2 and 255.
+	local CLIENT_ID="2"
+
+	# If a /etc/wireguard/basstoonvpn.conf already exists...
+	if [[ -f /etc/wireguard/basstoonvpn.conf ]]
 	then
-		echo "Cancelled."
-		exit 1
-	elif [ "$CHOSEN_OPTION" == "1" ]
-	then
-		# We want to generate the whole config from scratch, so remove the old one
-		DELETE_VPN_CONF="yes"
-	else
 		# Keep the old config, but extract the server private key and the next available client ID
 		while 
-			cat /etc/wireguard/basstoonvpn.conf | grep "192.168.103.$CLIENT_ID"
+			cat /etc/wireguard/basstoonvpn.conf | grep "192.168.103.$CLIENT_ID" >/dev/null
 		do
-			CLIENT_ID=$(($CLIENT_ID + 1))
+			local CLIENT_ID=$(($CLIENT_ID + 1))
 		done
-		if [ $CLIENT_ID -gt 255 ]
+		if [[ $CLIENT_ID -gt 255 ]]
 		then
-			echo "Error: There are too many clients already registered (you can have at most 253)."
-			exit 1
+			echo "Error: There are too many clients already registered (you can have at most 254)." 1>&2
+			return 1
 		fi
 		# I love sed. Don't you? Of course you do.
-		SERVER_PRIV_KEY=$(cat /etc/wireguard/basstoonvpn.conf | grep PrivateKey | sed -e "s/^PrivateKey\s*=\s*\([a-zA-Z0-9+\/\-_=]*\)\s*\$/\1/")
+		local SERVER_PRIV_KEY=$(cat /etc/wireguard/basstoonvpn.conf | grep PrivateKey | sed -e "s/^PrivateKey\s*=\s*\([a-zA-Z0-9+\/\-_=]*\)\s*\$/\1/")
+	else
+		# No VPN configuration found, this is probably a first install. Install prerequisites and remove firewalls that might mess with our setup
+		apt-get update -y >/dev/null
+		apt-get remove -y iptables firewalld 2> /dev/null >/dev/null || true
+		apt-get install -y wireguard-tools nftables curl >/dev/null
 	fi
-else
-	echo "==================================================="
-	echo "Setting up prerequisites. Just a moment..."
-	echo "==================================================="
-	# No VPN configuration found, this is probably a first install. Install prerequisites and remove firewalls that might mess with our setup
-	apt-get update -y
-	apt-get remove -y iptables firewalld 2> /dev/null || true
-	apt-get install -y wireguard-tools nftables curl qrencode
-fi
+	# Generate a fresh server private key if we didn't already extract one from an existing config. Then compute the public key from that.
+	if [[ -z "$SERVER_PRIV_KEY" ]]
+	then
+		local SERVER_PRIV_KEY="$(wg genkey)"
+	fi
+	local SERVER_PUB_KEY="$(echo $SERVER_PRIV_KEY | wg pubkey)"
 
+	# The preshared key is always server-generated, since the server needs knowledge of it anyway
+	local PRESHARED_KEY="$(wg genpsk)"
 
-# Server private key is generated fresh, unless it was already taken from an existing config
-if [ -z "$SERVER_PRIV_KEY" ]
-then
-	SERVER_PRIV_KEY="$(wg genkey)"
-fi
-SERVER_PUB_KEY="$(echo $SERVER_PRIV_KEY | wg pubkey)"
+	# For the client config, we need the server's public IP address somehow, and the ipify service is the simplest way to do that
+	local ENDPOINT=$(curl -s --fail https://api.ipify.org)
 
-echo ""
-echo "==================================================="
-echo "Creating new client..."
-echo "==================================================="
-echo ""
-echo "Would you like to:"
-echo "1) Generate a client private key on the server and print out a full client config (simpler, but less secure)"
-echo "2) Generate a client private key on the server and print out a QR code (useful for setting up smartphone clients)"
-echo "3) Enter only the client public key (more secure but you must generate the private key yourself and add it to your client config manually)"
-echo "c) Cancel the installation"
-CHOSEN_OPTION=""
-until [ "$CHOSEN_OPTION" = "1" -o "$CHOSEN_OPTION" = "2" -o "$CHOSEN_OPTION" = "3" -o "$CHOSEN_OPTION" = "c" ]
-do
-	read -p "Specify an option and press Enter (1, 2, 3, c): " CHOSEN_OPTION
-done
+	# Generate the client's config. The client will have to substiture their own private key in this.
+	local CLIENT_CONFIG=$(cat <<- EOF
+		[Interface]
+		PrivateKey = TODO-INSERT-YOUR-PRIVATE-KEY-HERE
+		Address = 192.168.103.$CLIENT_ID/24,fc00:8f78:4cb5:b7c4::$CLIENT_ID/64
+		DNS = 8.8.8.8,4.4.4.4
 
-PRINT_MODE="text"
-if [ "$CHOSEN_OPTION" == "c" ]
-then
-	echo "Cancelled."
-	exit 1
-elif [ "$CHOSEN_OPTION" == "1" ]
-then
-	CLIENT_PRIV_KEY="$(wg genkey)"
-	CLIENT_PUB_KEY="$(echo $CLIENT_PRIV_KEY | wg pubkey)"
-elif [ "$CHOSEN_OPTION" == "2" ]
-then
-	CLIENT_PRIV_KEY="$(wg genkey)"
-	CLIENT_PUB_KEY="$(echo $CLIENT_PRIV_KEY | wg pubkey)"
-	PRINT_MODE="qr"
-	echo "Warning: The QR code is fairly large. To ensure that it is printed correctly, you must zoom out your terminal."
-	echo "Scan the QR code BEFORE zooming back in to continue the installation. If it goes off the border of your terminal, it will get garbled."
-	read -p "Zoom out and press Enter."
-else
-	CLIENT_PRIV_KEY="PASTE-YOUR-PRIVATE-KEY-HERE"
-	while [ -z $CLIENT_PUB_KEY ]
-	do
-		read -p "Paste your client public key: " CLIENT_PUB_KEY
-	done
-fi
+		[Peer]
+		Endpoint = $ENDPOINT:51820
+		PublicKey = $SERVER_PUB_KEY
+		PresharedKey = $PRESHARED_KEY
+		AllowedIPs = 0.0.0.0/0,::/0
+		PersistentKeepalive = 25
+		EOF
+	)
 
-# The preshared key is always server-generated, since the server needs knowledge of it anyway
-PRESHARED_KEY="$(wg genpsk)"
-
-# For the client config, we need the server's public IP address somehow, and the ipify service is the simplest way to do that
-ENDPOINT=$(curl -s --fail https://api.ipify.org)
-
-CLIENT_CONFIG=$(cat <<- EOF
-[Interface]
-PrivateKey = $CLIENT_PRIV_KEY
-Address = 192.168.103.$CLIENT_ID/24, fc00:8f78:4cb5:b7c4::$CLIENT_ID/64
-DNS = 8.8.8.8,4.4.4.4
-
-[Peer]
-Endpoint = $ENDPOINT:51820
-PublicKey = $SERVER_PUB_KEY
-PresharedKey = $PRESHARED_KEY
-AllowedIPs = 0.0.0.0/5, 8.0.0.0/7, 11.0.0.0/8, 12.0.0.0/6, 16.0.0.0/4, 32.0.0.0/3, 64.0.0.0/2, 128.0.0.0/3, 160.0.0.0/5, 168.0.0.0/6, 172.0.0.0/12, 172.32.0.0/11, 172.64.0.0/10, 172.128.0.0/9, 173.0.0.0/8, 174.0.0.0/7, 176.0.0.0/4, 192.0.0.0/9, 192.128.0.0/11, 192.160.0.0/13, 192.169.0.0/16, 192.170.0.0/15, 192.172.0.0/14, 192.176.0.0/12, 192.192.0.0/10, 193.0.0.0/8, 194.0.0.0/7, 196.0.0.0/6, 200.0.0.0/5, 208.0.0.0/4, 224.0.0.0/3, ::/1, 8000::/2, c000::/3, e000::/4, f000::/5, f800::/6, fe00::/9, fec0::/10, ff00::/8, 192.168.103.0/24, fc00:8f78:4cb5:b7c4::0/64
-PersistentKeepalive = 25
-EOF
-)
-
-echo "==================================================="
-echo "Your client config will look like this:"
-echo "==================================================="
-echo ""
-if [ "$PRINT_MODE" = "qr" ]
-then
-	echo "$CLIENT_CONFIG" | qrencode -t ansi
-else
+	# Echo the config back
 	echo "$CLIENT_CONFIG"
-fi
-echo ""
-echo "==================================================="
-echo "You must paste it into your WireGuard client."
-echo "If you chose to generate a private key yourself, paste it where it says PASTE-YOUR-PRIVATE-KEY-HERE"
-echo "Note: Only one device can use this config. To add more devices, rerun $0."
-echo "==================================================="
 
-DO_INSTALL=""
-until [ "$DO_INSTALL" = "y" -o "$DO_INSTALL" = "n" ]
-do
-	read -p "Install this client? (y/n): " DO_INSTALL
-done
-if [ "$DO_INSTALL" != "y" ]
-then
-	echo "Cancelled."
-	exit 1
-fi
+	# If there was no config file or we asked to reset, overwrite the config file
+	if [[ ! (-f /etc/wireguard/basstoonvpn.conf) ]]
+	then
+	touch /etc/wireguard/basstoonvpn.conf
+	chown root:root /etc/wireguard/basstoonvpn.conf
+	chmod 0600 /etc/wireguard/basstoonvpn.conf
+	cat > /etc/wireguard/basstoonvpn.conf <<- EOF
+		[Interface]
+		PrivateKey = $SERVER_PRIV_KEY
+		Address = 192.168.103.1/24,fc00:8f78:4cb5:b7c4::1/64
+		ListenPort = 51820
+		PreUp = sysctl net.ipv4.ip_forward=1
+		PreUp = sysctl net.ipv6.conf.all.forwarding=1
+		PreUp = nft flush ruleset
+		PreUp = nft create table inet vpn_rules
+		PreUp = nft add chain inet vpn_rules prerouting "{ type nat hook postrouting priority 100; policy accept; }"
+		PreUp = nft add chain inet vpn_rules postrouting "{ type nat hook postrouting priority 100; policy accept; }"
+		PreUp = nft add rule inet vpn_rules postrouting iifname "%i" oifname != "%i" masquerade
+		PreUp = nft add chain inet vpn_rules input "{ type filter hook input priority 0; policy drop; }"
+		PreUp = nft add chain inet vpn_rules forward "{ type filter hook forward priority 0; policy drop; }"
+		PreUp = nft add chain inet vpn_rules output "{ type filter hook output priority 0; policy accept; }"
+		PreUp = nft add rule inet vpn_rules input ct state established,related accept
+		PreUp = nft add rule inet vpn_rules input udp dport 51820 accept
+		PreUp = nft add rule inet vpn_rules input iifname "%i" tcp dport 22 accept
+		PreUp = nft add rule inet vpn_rules input iifname "lo" accept
+		PreUp = nft add rule inet vpn_rules forward iifname "%i" accept
+		PreUp = nft add rule inet vpn_rules forward oifname "%i" accept
+		EOF
+	fi
 
-echo "==================================================="
-echo "Reconfiguring the VPN."
-echo "If you're using SSH, your connection might drop now. You need to activate your VPN client and reconnect to 192.168.103.1."
-echo "==================================================="
+	# Append the client's entry to the server config
+	cat >> /etc/wireguard/basstoonvpn.conf <<- EOF
 
-if [ "$DELETE_VPN_CONF" = "yes" ]
-then
-	rm /etc/wireguard/basstoonvpn.conf
-fi
+		[Peer]
+		PublicKey = $CLIENT_PUB_KEY
+		PresharedKey = $PRESHARED_KEY
+		AllowedIPs = 192.168.103.$CLIENT_ID/32, fc00:8f78:4cb5:b7c4::$CLIENT_ID/128
+		PersistentKeepalive = 25
+		EOF
+	
+	# Now start the service, trying not to disrupt existing VPN connections.
+	if
+		systemctl is-enabled --quiet wg-quick@basstoonvpn.service
+	then
+		if
+			systemctl is-active --quiet wg-quick@basstoonvpn.service
+		then
+			# Service enabled and running -> just reload to avoid disrupting existing connections
+			systemctl reload --quiet wg-quick@basstoonvpn.service
+		else
+			# Service is enabled, but not running -> start it
+			systemctl start --quiet wg-quick@basstoonvpn.service
+		fi
+	else
+		# Service is not enabled -> enable and start it
+		systemctl enable --now wg-quick@basstoonvpn.service
+	fi
 
-# Generate WireGuard config files. The PreUp commands in the server config will take care of firewall and forwarding rules.
+	# Done!
+	return 0
+}
+# Generate a key pair for the new client locally
+PRIV_KEY=$(openssl genpkey -algorithm X25519 -outform der | base64)
+PUB_KEY=$(echo $PRIV_KEY | base64 --decode | openssl pkey -pubout -outform der | base64)
+# Taking only the last 32 bytes of the DER representation gives us the raw keys, like WireGuard would use them
+PRIV_KEY=$(echo $PRIV_KEY | base64 --decode | tail -c 32 | base64)
+PUB_KEY=$(echo $PUB_KEY | base64 --decode | tail -c 32 | base64)
+# Now execute the operation on the remote and extract the output
+CLIENT_CONFIG=$(ssh -2 "$REMOTE_SSH_ADDR" "set -eo pipefail; $(declare -f add_wireguard_client); add_wireguard_client $PUB_KEY")
+# Substitute our private key in the result from the remote. We need to escape slashes in the private key otherwise they mess up the sed command.
+CLIENT_CONFIG=$(echo "$CLIENT_CONFIG" | sed -e "s/TODO-INSERT-YOUR-PRIVATE-KEY-HERE/$(echo $PRIV_KEY | sed -e 's/\//\\\//g')/")
 
-# Server base config (only printed if the file is missing)
-if [ ! -f /etc/wireguard/basstoonvpn.conf ]
-then
+print_separator
 
-touch /etc/wireguard/basstoonvpn.conf
-chown root:root /etc/wireguard/basstoonvpn.conf
-chmod 0600 /etc/wireguard/basstoonvpn.conf
+read -p "Config ready. I'm about to print a large QR code, so please zoom out your terminal and press Enter."
 
-cat > /etc/wireguard/basstoonvpn.conf <<- EOF
-[Interface]
-PrivateKey = $SERVER_PRIV_KEY
-Address = 192.168.103.1/24, fc00:8f78:4cb5:b7c4::1/64
-ListenPort = 51820
-PreUp = sysctl net.ipv4.ip_forward=1
-PreUp = sysctl net.ipv6.conf.all.forwarding=1
-PreUp = nft flush ruleset
-PreUp = nft create table inet vpn_rules
-PreUp = nft add chain inet vpn_rules prerouting "{ type nat hook postrouting priority 100; policy accept; }"
-PreUp = nft add chain inet vpn_rules postrouting "{ type nat hook postrouting priority 100; policy accept; }"
-PreUp = nft add rule inet vpn_rules postrouting iifname "%i" oifname != "%i" masquerade
-PreUp = nft add chain inet vpn_rules input "{ type filter hook input priority 0; policy drop; }"
-PreUp = nft add chain inet vpn_rules forward "{ type filter hook forward priority 0; policy drop; }"
-PreUp = nft add chain inet vpn_rules output "{ type filter hook output priority 0; policy accept; }"
-PreUp = nft add rule inet vpn_rules input ct state established,related accept
-PreUp = nft add rule inet vpn_rules input udp dport 51820 accept
-PreUp = nft add rule inet vpn_rules input iifname "%i" tcp dport 22 accept
-PreUp = nft add rule inet vpn_rules input iifname "lo" accept
-PreUp = nft add rule inet vpn_rules forward iifname "%i" accept
-PreUp = nft add rule inet vpn_rules forward oifname "%i" accept
-EOF
+print_separator
 
-fi
+echo "$CLIENT_CONFIG"
 
-# Append client entry to server config
-cat >> /etc/wireguard/basstoonvpn.conf <<- EOF
+print_separator
 
-[Peer]
-PublicKey = $CLIENT_PUB_KEY
-PresharedKey = $PRESHARED_KEY
-AllowedIPs = 192.168.103.$CLIENT_ID/32, fc00:8f78:4cb5:b7c4::$CLIENT_ID/128
-PersistentKeepalive = 25
-EOF
+echo "$CLIENT_CONFIG" | qrencode -t ansi
 
-# Make sure that any preexisting installation is stopped
-wg-quick down basstoonvpn 2> /dev/null || true
-systemctl disable --now wg-quick@basstoonvpn.service 2> /dev/null || true
-
-# Restart the VPN
-systemctl enable --now wg-quick@basstoonvpn.service
-
-echo "==================================================="
-echo "Done!"
-echo "==================================================="
+print_separator
